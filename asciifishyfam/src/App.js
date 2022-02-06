67a051c820d56185a5dba860f9cc5b172f1933b5
janis korisv <nooby4life19@gmail.com>
2022-02-05 21:03:18 -0500

    some social cards

--- asciifishyfam/src/App.js
+++ asciifishyfam/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
 import React from "react";
 import banner from './bannerAsciiTransparent.png';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons';
+
+
+
 // import BasicHooksExample  from './switchOption'
 
 function App() {
+
+  library.add(fab, faCheckSquare, faCoffee);
 
   
   
@@ -24,20 +32,46 @@ function App() {
         >
           <h3>SALE IS LIVE! CLICK HERE!</h3>
         </a>
+        <i class="fas fa-camera">Test</i>
         <h6>ROADMAP BEING INTRODUCED THIS MONTH!</h6>
         <h6>WEBSITE OVERHAUL BEING INTRODUCED</h6>
-        <h6>COMMUNITY POLLING OPTION POSSIBILITY</h6>
+        <h6>COMMUNITY POLLING OPTION</h6>
         </div>
-        <p>Ascii Fishy Fam is a collection of unique NFTs turned into ASCII art. What is ascii art? It's a style of art as old as the internet(literally) - It's a design technique that uses computers for presentation and consists of pictures pieced together from a total of 128 characters defined by the ASCII standard from 1963. Our Ascii Fishy Fam are entirely created using text! Don't believe me? Zoom into one! </p>
+        <p> Ascii Fishy Fam is a collection of unique NFTs turned into ASCII art. What is ascii art? It's a style of art as old as the internet(literally) - It's a design technique that uses computers for presentation and consists of pictures pieced together from a total of 128 characters defined by the ASCII standard from 1963. Our Ascii Fishy Fam are entirely created using text! Don't believe me? Zoom into one! </p>
+        
 
-        <a 
-        className='="App-link'
-        href="https://twitter.com/AsciiFishyFam"
-        target="_blank"
-        rel="noopener noreferrer"
-        >
-          Follow
-        </a>
+        <div class="cards">
+      <h1>
+ Social Media</h1>
+<div class="services">
+    <div class="content content-1">
+        <div class="fab fa-whatsapp"></div>
+<h2>
+OpenSea</h2>
+<p>
+Offer or buy from our expanding collection of Ascii Fishy Fam!</p>
+<a href="https://opensea.io/collection/the-ascii-fishies-fam">BUY</a>
+        </div>
+<div class="content content-2">
+          <div class="fab fa-whatsapp">
+</div>
+<h2>
+Twitter</h2>
+<p>
+Follow us on twitter to quickly find news and giveaways!</p>
+<a href="https://twitter.com/AsciiFishyFam">FOLLOW</a>
+        </div>
+<div class="content content-3">
+          <div class="fab fa-whatsapp">
+</div>
+<h2>
+TikTok</h2>
+<p>
+Follow us on TikTok to see Ascii Fishy Fam early releases!</p>
+<a href="https://www.tiktok.com/@asciifishyfam">Read More</a>
+        </div>
+</div>
+</div>
         
 <section class="sticky">
   <div class="bubbles">
