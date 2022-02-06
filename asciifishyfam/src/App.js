3a5e502eb169526a56b38f3b634d2c57c7077808
janis korisv <nooby4life19@gmail.com>
2022-02-05 21:54:57 -0500

    more stlying hjuhuhuhu

--- asciifishyfam/src/App.js
+++ asciifishyfam/src/App.js
@@ -24,17 +24,12 @@ function App() {
         <img src={banner} alt="ascii banner" width="70%" height="auto"/>
         <div className = "box">
         <h5>FISHY NEWS</h5>
-        <a
-          className="App-link"
-          href="https://opensea.io/collection/the-ascii-fishies-fam"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h3>SALE IS LIVE! CLICK HERE!</h3>
-        </a>
+        <h3>SALE IS LIVE! SEE SOCIAL MEDIA SECTION BELOW!</h3>
         <h6>ROADMAP BEING INTRODUCED THIS MONTH!</h6>
-        <h6>WEBSITE OVERHAUL BEING INTRODUCED</h6>
-        <h6>COMMUNITY POLLING OPTION</h6>
+        <h6>WEBSITE IMPROVEMENTS!</h6>
+        <h6>WALLET API INTERGRATION WITH WEBSITE!</h6>
+        <h6>OPENSEA API INTERGRATION WITH WEBSITE!</h6>
+        <h6>COMMUNITY POLLING OPTION!</h6>
         </div>
         <p> Ascii Fishy Fam is a collection of unique NFTs turned into ASCII art. What is ascii art? It's a style of art as old as the internet(literally) - It's a design technique that uses computers for presentation and consists of pictures pieced together from a total of 128 characters defined by the ASCII standard from 1963. Our Ascii Fishy Fam are entirely created using text! Don't believe me? Zoom into one! </p>
         
